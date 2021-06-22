064ca9dd866908f46cb6dac5d24b92086c275067
Shalin Okada <shalin.okada@wnd.co.jp>
2021-06-22 16:16:49 +0900

    :fire: Remove coment line

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,5 @@
 module.exports = {
   hooks: {
-    // 現在のコードは`tsc --noEmit`を実行するとエラーが多く出力されるので一時的にtscの確認を行わない
-    // 'pre-commit': 'tsc --noEmit && lint-staged'
-    'pre-commit': 'lint-staged',
+    'pre-commit': 'tsc --noEmit && lint-staged'
   },
 };
